25a484fd6ba29f43f29214551e02d8403b05b8a6
Christopher-John Cassar <christopher-john.cassar.16@ucl.ac.uk>
2020-10-20 17:44:25 -0400

    - Testing out node packages

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperheroName = superheroes.random();
+var mySupervillainName = supervillains.random();
+
+console.log(mySuperheroName);
+console.log(mySupervillainName);
